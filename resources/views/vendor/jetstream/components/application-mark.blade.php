44aad76666631ef271a3d4ef271840c04692a2c1
Aj <ajmartin81@gmail.com>
2021-05-28 10:50:26 +0200

    Logo update

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,4 +1,15 @@
-<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-  <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+<svg class="w-12 h-12" viewBox="0 0 160.67 160.55" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <style>.cls-1{fill:url(#Degradado_sin_nombre_39);}.cls-2{fill:#e8e8e8;}</style>
+    <linearGradient id="Degradado_sin_nombre_39" x1="-0.03" y1="160.51" x2="160.58" y2="-0.1" gradientUnits="userSpaceOnUse">
+      <stop offset="0.27" stop-color="#9669aa"/>
+      <stop offset="0.76" stop-color="#6f3e91"/>
+    </linearGradient>
+  </defs>
+  <g id="Capa_2" data-name="Capa 2">
+    <g id="Capa_1-2" data-name="Capa 1">
+      <path class="cls-1" d="M0,160.55V34.23S1.52,4.67,34.11,0H160.67V127s-3.53,30.31-34.61,33.6Z"/>
+      <path class="cls-2" d="M29,29H80.54V41.13h51.54V121s-.51,9.1-9.1,10.36c-8.08.5-84.12,0-84.12,0A14.42,14.42,0,0,1,29,123C28.09,116.2,29,29,29,29ZM41.81,41.56v26h77.05v-13H68.08l-.25-13Zm.13,77.56h76.8V80.27H41.94Z"/>
+    </g>
+  </g>
 </svg>
